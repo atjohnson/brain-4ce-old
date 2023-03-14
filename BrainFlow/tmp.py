8b798f4a8c638f6512ebdef14feeee1cb039faf5
Bshortall <shortall@bu.edu>
2023-03-13 22:27:52 -0400

    remove debugging tools and uneccessary code

--- BrainFlow/tmp.py
+++ BrainFlow/tmp.py
@@ -1,9 +1,13 @@
-import sys
-
-
 def main():
-    print(len(sys.argv))
-    print(sys.argv)
+    s = "start 1220"
+    print(s[6:len(s)])
+
+
+#board_shim.stop_stream() to stop streaming
+#get_current_board_data() - returns array of latest data stored in ringbuffer, does not clear ring buffer.
+#get_board_data(int count) - gets num of elements in ring buffer
+#insert_marker() - inserts marker to data stream
+#get_board_data() gets all data and removes from ringbuffer
 
 if __name__ == '__main__':
     main()
